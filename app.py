3d90dbff9cbcda4b23127704bb63eb19568c7722
Richi <tanrichi@hotmail.com>
2024-09-13 21:32:01 -0400

    done

---
+++ app.py
@@ -0,0 +1,19 @@
+import PySimpleGUI as sg
+
+sg.theme('Dark Brown')
+
+keyboardImage = sg.Image('new-keyboard-layout.png')
+
+layout = [[keyboardImage]]
+
+window = sg.Window('Keyboard Layout Helper', layout, location=(1100, 600),
+                   no_titlebar=True,  auto_size_buttons=False,
+                   keep_on_top=False, grab_anywhere=True,
+                   alpha_channel=0.5, use_default_focus=True)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+
+window.close()
